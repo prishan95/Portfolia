b5063d3d41feacdb0a70c41be31ffb79832f324e
PriShan <37224872+prishan95@users.noreply.github.com>
2023-08-30 15:39:00 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed("#text", {
+    strings:["Y&P is The Done", "Frontend Developer", "UI/UX Designer", "Responsive web"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
